e1f45310b570211e682913d8ad1a5958877828a5
shiningrise <shiningrise@qq.com>
2025-09-30 09:59:04 +0800

    josn支持编译运行通过

--- src/OSharp.Hosting.EntityConfiguration/Infos/MessageConfiguration.cs
+++ src/OSharp.Hosting.EntityConfiguration/Infos/MessageConfiguration.cs
@@ -6,6 +6,7 @@
 
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using OSharp.Entity.MySql;
 using OSharp.Hosting.Infos.Entities;
 using System.Text.Json;
 
@@ -22,7 +23,7 @@ namespace OSharp.Hosting.EntityConfiguration.Infos
         partial void EntityConfigurationAppend(EntityTypeBuilder<Message> builder)
         {
             //创建 Address 到 JSON 字符串的值转换器
-           var addressConverter = new ValueConverter<Address, string>(
+            var addressConverter = new ValueConverter<Address, string>(
                v => v == null ? null : JsonSerializer.Serialize(v, (JsonSerializerOptions)null),
                v => string.IsNullOrEmpty(v) ? null : JsonSerializer.Deserialize<Address>(v, (JsonSerializerOptions)null),
                new ConverterMappingHints(size: 4000));
@@ -30,10 +31,6 @@ namespace OSharp.Hosting.EntityConfiguration.Infos
             builder.Property(b => b.HomeAddress)
                 .HasConversion(addressConverter)
                 .HasColumnType("json");
-            //builder.OwnsOne(b => b.HomeAddress, a =>
-            //{
-            //    a.ToJson();
-            //});
         }
     }
 }
